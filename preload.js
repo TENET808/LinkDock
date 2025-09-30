946445d79c6aac003a8f6144904f7f9807dc0d34
TENET <65363393+TENET808@users.noreply.github.com>
2025-09-30 23:36:52 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -5,13 +5,23 @@ contextBridge.exposeInMainWorld('linkdock', {
   save: (key, value) => ipcRenderer.invoke('store:save', { key, value }),
   getTheme: () => ipcRenderer.invoke('ui:getTheme'),
   setTheme: (theme) => ipcRenderer.invoke('ui:setTheme', theme),
-  openLink: (url) => ipcRenderer.invoke('link:open', url),
+  
+  // ИСПРАВЛЕНО: Теперь соответствует вызову openExternalLink в вашем renderer.js
+  openExternalLink: (url) => ipcRenderer.invoke('link:open', url), 
+
   exportData: () => ipcRenderer.invoke('file:export'),
   on: (channel, callback) => {
     ipcRenderer.on(channel, (event, ...args) => callback(...args));
   },
-  // --- НОВЫЕ ФУНКЦИИ ДЛЯ ПРОВЕРКИ ССЫЛОК ---
-  checkAllLinks: () => ipcRenderer.invoke('links:checkAll'), // Запустить проверку всех ссылок
-  onLinkCheckProgress: (callback) => ipcRenderer.on('links:checkProgress', (event, ...args) => callback(...args)), // Получать прогресс проверки
-  onStartLinkCheck: (callback) => ipcRenderer.on('links:startCheck', (event, ...args) => callback(...args)) // Для запуска из меню main-процесса
+  
+  // ВЕРНУТО: Ваш IPC-вызов для модального окна удаления группы
+  showDeleteGroupDialog: (groupName) => ipcRenderer.invoke('dialog:showDeleteGroup', groupName),
+  
+  // ВЕРНУТО: Ваш IPC-вызов для импорта закладок
+  importBookmarks: (kind) => ipcRenderer.invoke('file:importBookmarks', kind),
+
+  // НОВЫЕ ФУНКЦИИ ДЛЯ ПРОВЕРКИ ССЫЛОК (оставляем, как обсуждали)
+  checkAllLinks: () => ipcRenderer.invoke('links:checkAll'),
+  onLinkCheckProgress: (callback) => ipcRenderer.on('links:checkProgress', (event, ...args) => callback(...args)),
+  onStartLinkCheck: (callback) => ipcRenderer.on('links:startCheck', (event, ...args) => callback(...args))
 });
